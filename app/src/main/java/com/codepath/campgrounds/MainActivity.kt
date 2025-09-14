dafeca2ab61d19bb2b96d5692bd45f6f49f6380a
Rmot1202 <mottraven43@gmail.com>
2025-09-14 17:43:43 -0400

    intial

--- app/src/main/java/com/codepath/campgrounds/MainActivity.kt
+++ app/src/main/java/com/codepath/campgrounds/MainActivity.kt
@@ -28,6 +28,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var campgroundsRecyclerView: RecyclerView
     private lateinit var binding: ActivityMainBinding
 
+    private val campgrounds = mutableListOf<Campground>()
+
     // TODO: Create campgrounds list
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,6 +42,8 @@ class MainActivity : AppCompatActivity() {
         campgroundsRecyclerView = findViewById(R.id.campgrounds)
 
         // TODO: Set up CampgroundAdapter with campgrounds
+        val campgroundAdapter = CampgroundAdapter(this, campgrounds)
+        campgroundsRecyclerView.adapter = campgroundAdapter
 
 
         campgroundsRecyclerView.layoutManager = LinearLayoutManager(this).also {
@@ -62,10 +66,18 @@ class MainActivity : AppCompatActivity() {
                 Log.i(TAG, "Successfully fetched campgrounds: $json")
                 try {
                     // TODO: Create the parsedJSON
+                    val parsedJson = createJson().decodeFromString(
+                        CampgroundResponse.serializer(),
+                        json.jsonObject.toString()
+                    )
+
 
                     // TODO: Do something with the returned json (contains campground information)
-
+                    parsedJson.data?.let { list ->
+                        campgrounds.addAll(list)
+                    }
                     // TODO: Save the campgrounds and reload the screen
+                    campgroundAdapter.notifyDataSetChanged()
 
                 } catch (e: JSONException) {
                     Log.e(TAG, "Exception: $e")
